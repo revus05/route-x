e1607b807dbc975290358b329aa203f40370021e
s0m3thln9 <skninni05@gmail.com>
2026-02-08 00:02:46 +0300

    feat: add cat to pilots page

--- src/components/PilotsPage.tsx
+++ src/components/PilotsPage.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import cat from "../../public/cat.jpg";
+
 const PilotsPage = () => {
-  return <span className={"text-center"}>В разработке</span>;
+  return (
+    <div className={"w-fit mx-auto flex flex-col gap-4 items-center"}>
+      <span className={"text-center"}>В разработке</span>
+      <Image src={cat.src} width={cat.width} height={cat.height} alt="cat" />
+    </div>
+  );
 };
 
 export default PilotsPage;
